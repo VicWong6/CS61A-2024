2f2f9843a10638b08590b282863f45297833692a
VicWong <616657066@qq.com>
2025-12-16 14:42:03 +0800

    finish lab02 Higher-Order Function Lamda and Expressions

---
+++ vic/lab/lab02/cake.py
@@ -0,0 +1,12 @@
+def cake():
+    print("beets")
+    def pie():
+        print("sweets")
+        return "cake"
+    return pie
+
+def snake(x, y):
+    if cake == more_cake:
+        return chocolate
+    else:
+        return x + y
